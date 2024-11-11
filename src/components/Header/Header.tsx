85a9f2320848b80129d7de753343ada0f57851ae
Gabriel Bastos <bastosgabriel35@hotmail.com>
2024-11-11 02:53:32 -0300

    responsividade mobile

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,35 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Header.module.scss';
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header className={styles.Header}>
-      <Link to="/">
-        <img src="logo.svg" alt="Logo" className={styles.logo} />
-      </Link>
-      <ul className={styles.options}>
+      <div className={styles.logoWrapper}>
+        <img src="logo.svg" alt="Animated Logo" className={styles.glitchGif} />
+        <Link to="/" className={styles.logoText}>DevStation</Link>
+      </div>
+      <div className={styles.hamburger} onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <ul className={`${styles.options} ${menuOpen ? styles.showMenu : ''}`}>
         <li className={styles.option}>
           <Link to="/Devs" className={styles.link}>Programadores</Link>
         </li>
         <li className={styles.option}>
           <Link to="/agendamento" className={styles.link}>Agendamento</Link>
+        </li>
+        {/* Aqui é onde estamos colocando a opção "Voltar" */}
+        <li className={`${styles.option} ${styles.centerOption}`}>
+          <Link to="/" className={styles.link}>Voltar</Link>
         </li>
       </ul>
     </header>
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -2,22 +2,28 @@ import styles from './Projects.module.scss';
 
 const Projects = () => {
     return (
-        <div className={styles.table}> 
-            <h2 className={styles.title}>Ideias de projeto para seu negócio</h2>
-            <section className={styles.Projects}>
-                <div className={styles.cards}>
-                    <h3 className={styles.titlecard}>Aplicativo mobile</h3>
-                    <img src="001.png" alt="Imagem do projeto" className={styles.projectimg} />
+        <div className={styles.phone}>
+            <div className={styles.mainWrapper}>
+                <div className={styles.content}>
+                    <div className={styles.headerContainer}>
+                        <h2 className={styles.title}>Ideias de Projeto para seu Negócio</h2>
+                    </div>
+                    <div className={styles.appsContainer}>
+                        <div className={styles.card}>
+                            <h3 className={styles.cardTitle}>Aplicativo Mobile</h3>
+                            <img src="001.png" alt="Aplicativo mobile" className={styles.cardImage} />
+                        </div>
+                        <div className={styles.card}>
+                            <h3 className={styles.cardTitle}>Site de Vendas</h3>
+                            <img src="003.png" alt="Site de vendas" className={styles.cardImage} />
+                        </div>
+                        <div className={styles.card}>
+                            <h3 className={styles.cardTitle}>Sistema de Gestão</h3>
+                            <img src="002.png" alt="Sistema de gestão" className={styles.cardImage} />
+                        </div>
+                    </div>
                 </div>
-                <div className={styles.cards}>
-                    <h3 className={styles.titlecard}>Site de vendas</h3>
-                    <img src="003.png" alt="Imagem do projeto" className={styles.projectimg} />
-                </div>
-                <div className={styles.cards}>
-                    <h3 className={styles.titlecard}>Sistema de gestão</h3>
-                    <img src="002.png" alt="Imagem do projeto" className={styles.projectimg} />
-                </div>
-            </section>
+            </div>
         </div>
     );
 };
